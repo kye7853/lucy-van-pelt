48cf33ba3ed849c53f992db9f17ee63d0836bb0f
sylim2357 <sylim2357@users.noreply.github.com>
2017-10-19 01:42:23 +0800

    no message

--- booklender.cpp
+++ booklender.cpp
@@ -2,17 +2,18 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 using namespace std;
 
-struct Borrowbooks{
+struct Book{
 	string title;
-	int pubyear;
+	int pubYear;
 	string author;
 	int edition;
 	string borrower;
-	int lentdays;
+	int lentDays;
 };
 
 const string delims(";,:. \n\t");
@@ -23,24 +24,52 @@ inline bool isDelim(char c) {
     return false;
 }
 
+inline string parseBookinfo(string s) {
+
+	string delimiter = "; ";
+	char* bookInfoArray = new string[6];
+	size_t pos = 0;
+	string info;
+	int j = 0;
+
+	while ((pos = s.find(delimiter)) != string::npos) {
+	    info = s.substr(0, pos);
+		bookInfoArray[j] = info;
+		j++;
+	   // cout << info << endl;
+	    s.erase(0, pos + delimiter.length());
+	}
+
+	Book bookInfo = {bookInfoArray[0], bookInfoArray[1], bookInfoArray[2], bookInfoArray[3], bookInfoArray[4], bookInfoArray[5]};
+	delete bookInfoArray;
+	tempBookList[lineCount] = bookInfo;
+
+}
+
 int main(int argc, char** argv) {
-	
-	string line[500];
-	int linecount = 0;			//to count the lines of the text file
-	
-	ifstream myfile("lenders.txt");		//input file stream
-	if (myfile.is_open())
+
+	//string line[500];
+	Book* tempBookList = new Book[200];
+	string bookInfoString = "";
+	int lineCount = 0;			//to count the lines of the text file
+
+	ifstream myFile("lenders.txt");		//input file stream
+	if (myFile.is_open())
 	{
-		for(int i=0; !myfile.eof(); i++) {
-			getline(myfile, line[i]);
-			cout << line[i] << "\n";
-	    	linecount++;
+		for(int i=0; !myFile.eof(); i++) {
+			getline(myFile,bookInfoString);
+			parseBookInfo(bookInfoString);
+			//getline(myfile, line[i]);
+			//cout << line[i] << "\n";
+	    	lineCount++;
 	    }
-		myfile.close();
+		myFile.close();
+		Book* bookList = new Book[lineCount];
+		copy(tempBookList,tempBookList[lineCount], bookList);
+		delete tempBookList;
+		printf(bookList[0]);
 	}
     else cout << "Unable to open file";		//message when file not opened
 
-	string delim = "; ";
-	
 	return 0;
 }

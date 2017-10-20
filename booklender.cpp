5fe939e901a253a66d862fc8d9e9051b4c593b15
sylim2357 <sylim2357@users.noreply.github.com>
2017-10-21 00:29:11 +0800

    load, insert, lend functions implemented.

--- booklender.cpp
+++ booklender.cpp
@@ -18,12 +18,13 @@ struct Book;
 /*Load*/
 void load();
 void loadBookFromTxtFile(ifstream& myFile);
-void parseBookInfo(string s, Book* tempBookList, int lineCount);
+void parseBookInfo(string s, int lineCount);
 /*passDay*/
 void passDay();
 
 /*Util*/
 ostream& operator<<(ostream &strm, const vector<Book> &v);
+ostream& operator<<(ostream &strm, const Book &b);
 vector<string> parseInput(string s, int i);
 
 /*variables*/
@@ -32,11 +33,17 @@ vector<Book> bookVector;
 /***********Book struct***********/
 struct Book {
 	string title;
-	string pubYear;
 	string author;
+	string pubYear;
 	string edition;
 	string borrower;
 	string lentDays;
+
+	Book(string title, string author, string pubYear, string edition, string borrower, string lentDays) :
+		title(title), author(author), pubYear(pubYear), edition(edition), borrower(borrower), lentDays(lentDays) {}
+
+	Book(string title, string author, string pubYear, string edition) :
+		title(title), author(author), pubYear(pubYear), edition(edition), borrower("None"), lentDays("0") {}
 };
 
 /***********Load***********/
@@ -50,22 +57,19 @@ void load() {
 	}
 	else cout << "Unable to open file";		//message when file not opened
 }
+
 void loadBookFromTxtFile(ifstream& myFile) {
-	Book* tempBookList = new Book[200];
 	string bookInfoString = "";
 	int lineCount = 0;
 
 	for (int i = 0; !myFile.eof(); i++) {
 		getline(myFile, bookInfoString);
-		parseBookInfo(bookInfoString, tempBookList, lineCount);
+		parseBookInfo(bookInfoString, lineCount);
 		lineCount++;
 	}
-
-	bookVector.resize(lineCount);
-	bookVector.assign(tempBookList, tempBookList + lineCount);
-	delete tempBookList;
 }
-void parseBookInfo(string s, Book* tempBookList, int lineCount) {
+
+void parseBookInfo(string s, int lineCount) {
 
 	string delimiter = "; ";
 	vector<string> bookInfoVector(6);
@@ -74,11 +78,67 @@ void parseBookInfo(string s, Book* tempBookList, int lineCount) {
 	size_t next = 0;
 
 	bookInfoVector = parseInput(s, 6);
-	Book bookInfo = {bookInfoVector[0], bookInfoVector[1], bookInfoVector[2], bookInfoVector[3], bookInfoVector[4], bookInfoVector[5]};
-	tempBookList[lineCount] = bookInfo;
+	Book book = {bookInfoVector[0], bookInfoVector[1], bookInfoVector[2], bookInfoVector[3], bookInfoVector[4], bookInfoVector[5]};
+	bookVector.resize(lineCount, book);
 
 }
 
+/***********INSERT BookTitle; Author; PubYear; Edition***********/
+void insert(string s) {
+	vector<string> insertBookInfoVector(4);
+	insertBookInfoVector = parseInput(s, 4);
+
+	Book newBook = {insertBookInfoVector[0], insertBookInfoVector[1], insertBookInfoVector[2], insertBookInfoVector[3]};
+	bookVector.resize(bookVector.size() + 1, newBook);
+	cout << bookVector;
+}
+
+/***********LEND BookTitle; Person Borrowing; How many days***********/
+void lend(string s) {
+	vector<string> lendBookInfoVector(3);
+	string bookTitle = lendBookInfoVector[0];
+	vector<Book>::iterator it = find_if(bookVector.begin(), bookVector.end(), [&bookTitle](const Book& obj) {return obj.title == bookTitle;});
+	string errorMessage = "";
+
+	if (stoi(lendBookInfoVector[2]) < 1) {
+		errorMessage.append("Lend for more days!");
+	}
+
+	if (it != bookVector.end())
+	{
+		if (errorMessage.empty()) {
+			(*it).borrower = lendBookInfoVector[1];
+			(*it).lentDays = lendBookInfoVector[2];
+			//int index = distance(bookVector.begin(), it);
+			cout << *it;
+		}
+	}
+	else {
+		errorMessage.append("\n NO SUCH BOOK!");
+	}
+
+	if (!errorMessage.empty()) {
+		cout << errorMessage;
+	}
+}
+/***********SAVE new_filename.txt***********/
+/***********RETURNED BookTitle***********/
+/***********PASSDAY***********/
+void passDay() {
+	int lentDaysInt = 0;
+	for (int i = 0; i < bookVector.size(); i++) {
+		lentDaysInt = stoi(bookVector[i].lentDays);
+		lentDaysInt -= 1;
+		if (lentDaysInt < 0) {
+
+		}
+		bookVector[i].lentDays = to_string(lentDaysInt);
+	}
+}
+/***********PRINT***********/
+/***********EXIT***********/
+
+/***********Utils***********/
 vector<string> parseInput(string s, int i) {
 	string delimiter = "; ";
 	int j = 0;
@@ -96,33 +156,20 @@ vector<string> parseInput(string s, int i) {
 	return bookInfoVector;
 }
 
-/***********INSERT BookTitle; Author; PubYear; Edition***********/
-/***********LEND BookTitle; Person Borrowing; How many days***********/
-/***********SAVE new_filename.txt***********/
-/***********RETURNED BookTitle***********/
-/***********PASSDAY***********/
-void passDay() {
-	int lentDaysInt = 0;
-	for (int i = 0; i < bookVector.size(); i++) {
-		lentDaysInt = stoi(bookVector[i].lentDays);
-		lentDaysInt -= 1;
-		bookVector[i].lentDays = to_string(lentDaysInt);
-	}
-}
-/***********PRINT***********/
-/***********EXIT***********/
-
-/***********Utils***********/
 ostream& operator<<(ostream &strm, const vector<Book> &v) {
 	for (int i = 0; i < v.size() ; i++) {
-		Book b = v[i];
+		strm << v[i];
+	}
+	return strm;
+}
+
+ostream& operator<<(ostream &strm, const Book &b) {
 		strm << "title: " << b.title <<
 			"\t pubYear: " << b.pubYear <<
 			"\t author: " << b.author <<
 			"\t edition: " << b.edition <<
 			"\t borrower: " << b.borrower <<
 			"\t lentDays: " << b.lentDays << "\n";
-	}
 	return strm;
 }
 /***********Main***********/

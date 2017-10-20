b98b7839903011d0b26526448873c36317cfc449
sylim2357 <sylim2357@users.noreply.github.com>
2017-10-20 21:44:01 +0800

    no message

--- booklender.cpp
+++ booklender.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -10,7 +11,23 @@
 
 using namespace std;
 
-struct Book{
+/*Struct*/
+struct Book;
+
+/*Functions*/
+/*Load*/
+void load();
+void parseBookInfo(string s, Book* tempBookList, int lineCount);
+/*passDay*/
+void passDay();
+/*Util*/
+ostream& operator<<(ostream &strm, const vector<Book> &v);
+
+/*variables*/
+vector<Book> bookVector;
+
+/***********Book struct***********/
+struct Book {
 	string title;
 	string pubYear;
 	string author;
@@ -19,44 +36,8 @@ struct Book{
 	string lentDays;
 };
 
-const string delims(";,:. \n\t");
-inline bool isDelim(char c) {
-    for (int i = 0; i < delims.size(); ++i)
-        if (delims[i] == c)
-            return true;
-    return false;
-}
-
-inline void parseBookInfo(string s,Book* tempBookList,int lineCount) {
-
-	string delimiter = "; ";
-	vector<string> bookInfoVector(3);
-//	char* bookInfoArray = new char[6];
-	size_t pos = 0;
-	string info;
-	int j = 0;
-
-	while ((pos = s.find(delimiter)) != string::npos) {
-	    info = s.substr(0, pos);
-		bookInfoVector[j] = info;
-	//	char* infoChar = new char[info.length()];
-	//	strcpy(infoChar, info);
-	//	bookInfoArray[j] = infoChar;
-	//	delete infoChar;
-		j++;
-	   // cout << info << endl;
-	    s.erase(0, pos + delimiter.length());
-	}
-
-	Book bookInfo = {bookInfoVector[0], bookInfoVector[1], bookInfoVector[2], bookInfoVector[3], bookInfoVector[4], bookInfoVector[5]};
-	//delete bookInfoArray;
-	tempBookList[lineCount] = bookInfo;
-
-}
-
-int main(int argc, char** argv) {
-
-	//string line[500];
+/***********Load***********/
+void load() {
 	Book* tempBookList = new Book[200];
 	string bookInfoString = "";
 	int lineCount = 0;			//to count the lines of the text file
@@ -64,23 +45,72 @@ int main(int argc, char** argv) {
 	ifstream myFile("lenders.txt");		//input file stream
 	if (myFile.is_open())
 	{
-		for(int i=0; !myFile.eof(); i++) {
-			getline(myFile,bookInfoString);
-			parseBookInfo(bookInfoString,tempBookList,lineCount);
-			//getline(myfile, line[i]);
-			//cout << line[i] << "\n";
-	    	lineCount++;
-	    }
+		for (int i = 0; !myFile.eof(); i++) {
+			getline(myFile, bookInfoString);
+			parseBookInfo(bookInfoString, tempBookList, lineCount);
+			lineCount++;
+		}
 		myFile.close();
-		vector<Book> bookList(lineCount);
-		//Book* bookList = new Book[lineCount];
-	//	copy(tempBookList[0],tempBookList[lineCount], bookList);
-	//	delete tempBookList;
-	//	cout <<bookList[0].title;
-	cout << tempBookList;
-	cout << "H";
+		bookVector.resize(lineCount);
+		bookVector.assign(tempBookList, tempBookList + lineCount);
+		delete tempBookList;
+		cout << bookVector;
 	}
-    else cout << "Unable to open file";		//message when file not opened
+	else cout << "Unable to open file";		//message when file not opened
+}
+
+void parseBookInfo(string s, Book* tempBookList, int lineCount) {
+
+	string delimiter = "; ";
+	vector<string> bookInfoVector(6);
+	int j = 0;
+	size_t last = 0;
+	size_t next = 0;
+
+	while ((next = s.find(delimiter, last)) != string::npos) {
+		bookInfoVector[j] = s.substr(last, next - last);
+		last = next + 1;
+		j++;
+	}
+	bookInfoVector[j] = s.substr(last);
+
+	Book bookInfo = {bookInfoVector[0], bookInfoVector[1], bookInfoVector[2], bookInfoVector[3], bookInfoVector[4], bookInfoVector[5]};
+	tempBookList[lineCount] = bookInfo;
+
+}
+
+/***********INSERT BookTitle; Author; PubYear; Edition***********/
+/***********LEND BookTitle; Person Borrowing; How many days***********/
+/***********SAVE new_filename.txt***********/
+/***********RETURNED BookTitle***********/
+/***********PASSDAY***********/
+void passDay() {
+	int lentDaysInt = 0;
+	for (int i = 0; i < bookVector.size(); i++) {
+		lentDaysInt = stoi(bookVector[i].lentDays);
+		lentDaysInt -= 1;
+		bookVector[i].lentDays = to_string(lentDaysInt);
+	}
+}
+/***********PRINT***********/
+/***********EXIT***********/
+
+/***********Utils***********/
+ostream& operator<<(ostream &strm, const vector<Book> &v) {
+	for (int i = 0; i < v.size() ; i++) {
+		Book b = v[i];
+		strm << "title: " << b.title <<
+			"\t pubYear: " << b.pubYear <<
+			"\t author: " << b.author <<
+			"\t edition: " << b.edition <<
+			"\t borrower: " << b.borrower <<
+			"\t lentDays: " << b.lentDays << "\n";
+	}
+	return strm;
+}
+/***********Main***********/
+int main(int argc, char** argv) {
+
 
 	return 0;
 }
